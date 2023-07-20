a3997742b97b915e70c8a6d358bd4542c4923b27
mdpotter31 <mdpotter31@gmail.com>
2023-07-19 21:31:24 -0400

    Added back the js for the form.

--- js/main.js
+++ js/main.js
@@ -7,6 +7,53 @@ navBarToggle.addEventListener("click", function () {
 
 
 
+// Form
+// Set minimum and maximum dates for date input
+var currentDate = new Date();
+var minDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + 2);
+var maxDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + 21);
+
+document.getElementById('date').min = minDate.toISOString().split('T')[0];
+document.getElementById('date').max = maxDate.toISOString().split('T')[0];
+
+// Apply event listeners and validation on form submission
+document.querySelector('form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  // Check if all required fields are filled
+  var requiredFields = document.querySelectorAll('input[required], select[required], textarea[required]');
+  var isFormValid = true;
+
+  for (var i = 0; i < requiredFields.length; i++) {
+    if (!requiredFields[i].value) {
+      isFormValid = false;
+      requiredFields[i].classList.add('error');
+    } else {
+      requiredFields[i].classList.remove('error');
+    }
+  }
+
+  // Additional validation to prevent selecting weekends
+  var selectedDate = new Date(document.getElementById('date').value);
+  var dayOfWeek = selectedDate.getDay();
+  if (dayOfWeek === 0 || dayOfWeek === 6) { // Sunday (0) and Saturday (6)
+    isFormValid = false;
+    document.getElementById('date').classList.add('error');
+  } else {
+    document.getElementById('date').classList.remove('error');
+  }
+
+  // Display notification message based on form submission
+  if (isFormValid) {
+    alert('Thank you for submitting. Our dispatcher will call you within the next three days to confirm your schedule.');
+    document.querySelector('form').reset();
+  } else {
+    alert('Please fill out all required fields and select a valid weekday date.');
+  }
+});
+
+
+
 
 
 //Testimonials
